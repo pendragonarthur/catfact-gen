f3c51e2e877f5eb09bd5cc4195be4bd530c1017a
tuzinmaibrabo <arthurquarxma@outlook.com>
2023-03-07 17:49:11 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import Axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.sass";
+import { BsGithub } from "react-icons/bs";
 
 function App() {
+  const [catFact, setCatFact] = useState("");
+
+  const fetchCatFact = () => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+      setCatFact(res.data.fact);
+    });
+  };
+
+  useEffect(() => {
+    fetchCatFact();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="title-container">
+        <h1>CatFact Generator!</h1>
+        <p>Click on the button 'Meow' to generate cat fact.</p>
+      </div>
+      <div className="cat-container">
+        <button onClick={fetchCatFact}>Meow!</button>
+        <p>{catFact}</p>
+      </div>
+      <div className="github-icon">
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="http://github.com/pendragonarthur"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <BsGithub />
+          <p>Arthur Quaresma</p>
         </a>
-      </header>
+      </div>
     </div>
   );
 }
